66241640033eaac4886a0d8bfd8467ee07803683
DON-25 <2017715353@qq.com>
2025-07-14 21:33:54 +0800

    rules.py

--- rules.py
+++ rules.py
@@ -1,6 +1,7 @@
 #文件分类规则逻辑
 from pathlib import Path
-def match_category(file:Path,rules:dict)-> str|None:
+from typing import Optional
+def match_category(file: Path, rules: dict) -> Optional[str]:
     suffix = file.suffix.lower()
     for category,extensions in rules.items():
          if suffix in extensions:
